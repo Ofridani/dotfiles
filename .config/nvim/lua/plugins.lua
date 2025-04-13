f98e7515227b99c44acb13ee107f5a99217ef162
Ofridani <ofridaniel100@gmail.com>
2025-04-13 16:26:57 +0300

    Added cool neovim color scheme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -12,21 +12,31 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- UI
-  { "nvim-lualine/lualine.nvim" },
-  { "nvim-tree/nvim-tree.lua" },
-  { "nvim-tree/nvim-web-devicons" },
+    -- UI
+    { "nvim-lualine/lualine.nvim" },
+    { "nvim-tree/nvim-tree.lua" },
+    { "nvim-tree/nvim-web-devicons" },
 
-  -- Fuzzy finding
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+    -- Fuzzy finding
+    { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 
-  -- Syntax highlighting
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    -- Syntax highlighting
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-  -- LSP and completion
-  { "neovim/nvim-lspconfig" },
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "L3MON4D3/LuaSnip" },
+    -- LSP and completion
+    { "neovim/nvim-lspconfig" },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "L3MON4D3/LuaSnip" },
+
+    -- Color scheme: tokyonight
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.cmd.colorscheme("tokyonight")
+      end,
+    },
 })
 
